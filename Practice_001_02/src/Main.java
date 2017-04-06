8ebe80e03b9e0de8b49b1264a1d4c4fbe9020c5b
Raymounde <a0falcon@gmail.com>
2017-04-06 18:56:14 +0800

    Create program that check if the value is divisible by 2 or 3

---
+++ Practice_001_02/src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Alfie on 4/5/2017.
+ */
+public class Main {
+
+    public static void main(String Args[]) {
+
+        /*
+         * PROBLEM 01:
+         *  Print "Cola" if divisible by 2
+         *  Print "Pepsi" if divisible by 3
+         *  Print "PepsiCola" if divisible by 2 and 3
+         *  Print "Not divisible by 2 or 3" if not of the above
+         */
+        PepsiCola pepsiCola = new PepsiCola();
+
+        pepsiCola.PrintPepsiCola(3);
+        pepsiCola.PrintPepsiCola(2);
+        pepsiCola.PrintPepsiCola(6);
+        pepsiCola.PrintPepsiCola(7);
+    }
+}
---
+++ Practice_001_02/src/PepsiCola.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Alfie on 4/5/2017.
+ */
+public class PepsiCola {
+
+    // Check value to determine what to print
+    public void PrintPepsiCola(Integer value) {
+        boolean divisibleByTwo = (value % 2 == 0) ? true : false;
+        boolean divisibleByThree = (value % 3 == 0) ? true : false;
+
+        if (divisibleByTwo && divisibleByThree) {
+            System.out.println("PepsiCola");
+        }
+        else if (divisibleByTwo) {
+            System.out.println("Cola");
+        }
+        else if (divisibleByThree) {
+            System.out.println("Pepsi");
+        }
+        else {
+            System.out.println("Not divisible by 2 or 3");
+        }
+    }
+}
